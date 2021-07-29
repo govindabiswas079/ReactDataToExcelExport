2f458e4261d5464b9387fc6cea9840aca4d42c32
prem-pb <govindbiswas079@gmail.com>
2021-07-29 10:49:04 +0530

    first commit

---
+++ src/ApiData.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+//import data from './data';
+
+import ReactHtmlTableToExcel from 'react-html-table-to-excel';
+
+const ApiData = () => {
+    const [users, setUsers] = useState([]);
+
+    const UserData = async () => {
+        const result = await axios.get('http://localhost:3001/users');
+        setUsers(result.data);
+        /* console.log(result); */
+
+    }
+
+    useEffect(() => {
+        UserData();
+    }, []);
+
+    const deleteUsers = async id => {
+        await axios.delete(`http://localhost:3001/users/${id}`);
+        UserData();
+    };
+
+    return (
+        <div className="py-4 container">
+            <h1 className="text-center text-info mt-3">All User's</h1>
+            <div className="row justify-content-center">
+                <table className="table table-striped" id="data-table">
+                    <thead>
+                        <tr>
+                            <td>Sr.</td>
+                            <td>Name</td>
+                            <td>User Name</td>
+                            <td>Email</td>
+                            <td>Phone</td>
+                            <td>Website</td>
+                            <td>Action</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map((items, index) => (
+                            <tr key={index}>
+                                <td>{items.id}</td>
+                                <td>{items.name}</td>
+                                <td>{items.username}</td>
+                                <td>{items.email}</td>
+                                <td>{items.phone}</td>
+                                <td>{items.website}</td>
+                                <td>
+                                    <button className="btn btn-outline-primary" onClick={() => deleteUsers(items.id)}>Delete</button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+                <ReactHtmlTableToExcel
+                    className="btn btn-info"
+                    table="data-table"
+                    filename="Data Excel File"
+                    sheet="Sheet"
+                    buttonText="Export to Excel"
+                />
+            </div>
+        </div>
+    )
+}
+
+export default ApiData;
---
+++ src/data.js
@@ -0,0 +1,156 @@
+const data = [
+    {
+        "name": "Prem Biswas",
+        "username": "prem@",
+        "email": "govindbiswas079@gmail.com",
+        "phone": "09511723507",
+        "website": "biswasmotors.com",
+        "id": 16
+    },
+    {
+        "name": "Amit Amol Biswas",
+        "username": "Amitbiswas@",
+        "email": "abiswas01@gmail.com",
+        "phone": "7890654321",
+        "website": "example.com",
+        "id": 17
+    },
+    {
+        "name": "Kapil Biswas ",
+        "username": "kapil@",
+        "email": "kbiswas@gmail.com",
+        "phone": "09511723507",
+        "website": "example.com",
+        "id": 18
+    },
+    {
+        "name": "tejes biswas ",
+        "username": "tbiswas@",
+        "email": "tb@gmail.com",
+        "phone": "9087654321",
+        "website": "tb.com",
+        "id": 19
+    },
+    {
+        "name": "Kapil Biswas ",
+        "username": "kapil@",
+        "email": "kbiswas@gmail.com",
+        "phone": "09511723507",
+        "website": "example.com",
+        "id": 20
+    },
+    {
+        "name": "Kapil Biswas ",
+        "username": "kapil@",
+        "email": "kbiswas@gmail.com",
+        "phone": "09511723507",
+        "website": "example.com",
+        "id": 21
+    },
+    {
+        "name": "Kapil Biswas ",
+        "username": "kapil@",
+        "email": "kbiswas@gmail.com",
+        "phone": "09511723507",
+        "website": "example.com",
+        "id": 22
+    },
+    {
+        "name": "Kapil Biswas ",
+        "username": "kapil@",
+        "email": "kbiswas@gmail.com",
+        "phone": "09511723507",
+        "website": "example.com",
+        "id": 23
+    },
+    {
+        "name": "Kapil Biswas ",
+        "username": "kapil@",
+        "email": "kbiswas@gmail.com",
+        "phone": "09511723507",
+        "website": "example.com",
+        "id": 24
+    },
+    {
+        "name": "Kapil Biswas ",
+        "username": "kapil@",
+        "email": "kbiswas@gmail.com",
+        "phone": "09511723507",
+        "website": "example.com",
+        "id": 25
+    },
+    {
+        "name": "Kapil Biswas ",
+        "username": "kapil@",
+        "email": "kbiswas@gmail.com",
+        "phone": "09511723507",
+        "website": "example.com",
+        "id": 26
+    },
+    {
+        "name": "Kapil Biswas ",
+        "username": "kapil@",
+        "email": "kbiswas@gmail.com",
+        "phone": "09511723507",
+        "website": "example.com",
+        "id": 27
+    },
+    {
+        "name": "Kapil Biswas ",
+        "username": "kapil@",
+        "email": "kbiswas@gmail.com",
+        "phone": "09511723507",
+        "website": "example.com",
+        "id": 28
+    },
+    {
+        "name": "Kapil Biswas ",
+        "username": "kapil@",
+        "email": "kbiswas@gmail.com",
+        "phone": "09511723507",
+        "website": "example.com",
+        "id": 29
+    },
+    {
+        "name": "Kapil Biswas ",
+        "username": "kapil@",
+        "email": "kbiswas@gmail.com",
+        "phone": "09511723507",
+        "website": "example.com",
+        "id": 30
+    },
+    {
+        "name": "Kapil Biswas ",
+        "username": "kapil@",
+        "email": "kbiswas@gmail.com",
+        "phone": "09511723507",
+        "website": "example.com",
+        "id": 31
+    },
+    {
+        "name": "Kapil Biswas ",
+        "username": "kapil@",
+        "email": "kbiswas@gmail.com",
+        "phone": "09511723507",
+        "website": "example.com",
+        "id": 32
+    },
+    {
+        "name": "Kapil Biswas ",
+        "username": "kapil@",
+        "email": "kbiswas@gmail.com",
+        "phone": "09511723507",
+        "website": "example.com",
+        "id": 33
+    },
+    {
+        "name": "Kapil Biswas ",
+        "username": "kapil@",
+        "email": "kbiswas@gmail.com",
+        "phone": "09511723507",
+        "website": "example.com",
+        "id": 34
+    }
+]
+
+export default data;
